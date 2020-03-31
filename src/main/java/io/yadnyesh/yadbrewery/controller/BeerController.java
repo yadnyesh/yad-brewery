a831a80b3f5a831384c0e18a3f51f2c92390f81c
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-03-31 09:44:20 +0530

    Implementation for Beer Object

--- src/main/java/io/yadnyesh/yadbrewery/controller/BeerController.java
+++ src/main/java/io/yadnyesh/yadbrewery/controller/BeerController.java
@@ -2,6 +2,7 @@ package io.yadnyesh.yadbrewery.controller;
 
 import io.yadnyesh.yadbrewery.model.BeerDto;
 import io.yadnyesh.yadbrewery.service.BeerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +22,13 @@ public class BeerController {
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
 		return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<BeerDto> addBeer(@RequestBody BeerDto beerDto) {
+		BeerDto savedBeerDto = beerService.saveNewBeer(beerDto);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "/api/v1/beer" + savedBeerDto.getId().toString());
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 	}
 }
--- src/main/java/io/yadnyesh/yadbrewery/service/BeerService.java
+++ src/main/java/io/yadnyesh/yadbrewery/service/BeerService.java
@@ -7,4 +7,5 @@ import java.util.UUID;
 public interface BeerService {
 	
 	BeerDto getBeerById(UUID beerId);
+	BeerDto saveNewBeer(BeerDto beerDto);
 }
