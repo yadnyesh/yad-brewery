abf2aa660447dda5cdd1e14d3db508ca224aadc9
Yadnyesh <yadnyesh.juvekar@gmail.com>
2020-04-04 02:51:28 +0530

    Bean Validation Beer and Customer objects

--- src/main/java/io/yadnyesh/yadbrewery/controller/v2/BeerControllerV2.java
+++ src/main/java/io/yadnyesh/yadbrewery/controller/v2/BeerControllerV2.java
@@ -56,14 +56,4 @@ public class BeerControllerV2 {
 		beerServiceV2.deleteById(beerId);
 	}
 	
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e){
-		List<String> errors = new ArrayList<>(e.getConstraintViolations().size());
-		
-		e.getConstraintViolations().forEach(constraintViolation -> {
-			errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
-		});
-		
-		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-	}
 }
